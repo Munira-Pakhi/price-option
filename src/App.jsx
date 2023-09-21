4815d57a9e6d21c3685b645e0cc807e180f2366b
Munira-Pakhi <khatunmunira20@gmail.com>
2023-09-21 23:53:32 +0600

    navbar section done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import PriceOptions from './components/PriceOptions/PriceOptions'
+
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+      {/* <DaisyNav></DaisyNav> */}
+      <h1 className='text-7xl bg-rose-600 text-white'>Vite + React</h1>
+      <PriceOptions></PriceOptions>
+    </>
+  )
+}
+
+export default App
